136fa06b49b238f34f1b1f6a0700f4320539eb0c
Arondeke <khmelnitskiy.aleksey@gmail.com>
2023-01-23 19:14:21 +0300

    Сделано ДЗ(ветка hw_7 длянового коммита)

--- src/Main.java
+++ src/Main.java
@@ -112,6 +112,7 @@ public class Main {
 
     public static void task8() {
         System.out.println("Задача 8");
+        int forNewCommit = 0; //для нового коммита
         int yearCommet = 0;
         int yearNow = 2023;
 
